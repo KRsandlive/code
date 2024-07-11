f86157a6487acb8011c23e407a18261766b72e74
sandlive <wkri4731@gmail.com>
2024-07-11 15:25:33 +0900

    rr

--- code/main.js
+++ code/main.js
@@ -4,7 +4,7 @@ import { readFile } from 'fs'
 const app = express()
 
 app.get('/', (request, response) => {
-  readFile('index.html', 'utf-8', (err, data) => {
+  readFile('a.html', 'utf-8', (err, data) => {
     if(err) { response.send('No Such File of Directory') }
     response.send(data)
   })
