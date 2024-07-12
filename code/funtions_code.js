0ce0c970b50561109d122f11382a155be0fa92d5
sandlive <wkri4731@gmail.com>
2024-07-12 19:50:11 +0900

    푸시됨 최신버진

--- code/funtions_code.js
+++ code/funtions_code.js
@@ -1,8 +1,17 @@
+const times = 1;
+var cloker_free;
+let time_on = 0;
 function setClock(){
-   var clockL
-   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0"); 
-  const seconds = String(date.getSeconds()).padStart(2, "0"); 
-  clockL = hours + ':' + minutes + ':' + seconds;
+  const now = new Date();
+  let hours = now.getHours();
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+  if(hours <= 12) {hours = '오전 ' + String(Number(hours))}
+  if(Number(hours) > 12);{hours = '오후 ' +String(Number(hours) - 12)}
+  cloker_free = `${hours}시 ${minutes}분 ${seconds}초`;
+  document.getElementById('clocker').textContent = cloker_free;
+  time_on_text = (`켜진지 ${time_on}초 지남`); time_on = Number(time_on + 1);
+  document.getElementById('time_on').textContent = time_on_text;
 }
+setInterval(setClock, 1000);
+setClock();
