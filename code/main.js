103cbf17a3577868080be4aecc0278d7cadb0df6
sandlive <83458520+KRsandlive@users.noreply.github.com>
2026-03-26 22:49:09 +0900

    fuck you

--- code/main.js
+++ code/main.js
@@ -1,90 +1,76 @@
-import express, { request, response } from 'express'
-import {readFile} from "fs";
+import express from 'express'
 import fs from 'fs'
-import cors, {} from 'cors'
-import path, {} from 'path'
-import { fileURLToPath } from "url";   
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from "url"
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));   
-const __filename = fileURLToPath(import.meta.url);
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const app = express()
 
 app.use(cors())
+// 정적 파일 제공 (public 폴더 안에 있으면 자동으로 불러옴)
 app.use(express.static(path.join(__dirname, 'public')));
 
-var accounts_password_t;
-var var_acount; 
-var k;
-var count = 0;
-var count_val = 0;
-var message_1 = "0";
+// 서버 켤 때 계정 파일 딱 한 번 읽어오기
+const accountFile = fs.readFileSync(path.join(__dirname, 'accounts/accounts.json'), 'utf8');
+const accountData = JSON.parse(accountFile);
 
-
-const accountFile = fs.readFileSync('accounts/accounts.json', 'utf8'); 
-const accountData = JSON.parse(accountFile); 
-
+// 메인 페이지
 app.get('/', (request, response) => {
-  readFile('index.html', 'utf-8', (err, data) => {
-    if(err) { response.send('파일 또는 디렉터리가 잘못됨') }
-    response.send(data)
-  })
-})
+    fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', (err, data) => {
+        if (err) return response.send('파일 또는 디렉터리가 잘못됨');
+        response.send(data);
+    });
+});
 
+// 옛날 사이트
 app.get('/old_site', (request, response) => {
-  readFile('old_site/index.html', 'utf-8', (err, data) => {
-    if(err) { response.send('파일 또는 디렉터리가 잘못됨') }
-    response.send(data)
-  })
-})
+    fs.readFile(path.join(__dirname, 'old_site/index.html'), 'utf-8', (err, data) => {
+        if (err) return response.send('파일 또는 디렉터리가 잘못됨');
+        response.send(data);
+    });
+});
 
+// 자바스크립트 파일 직접 서빙
 app.get('/functions_code.js', (request, response) => {
-  readFile('functions_code.js', 'utf-8', (err, data) => {
-    if(err) { response.send('파일 또는 디렉터리가 잘못됨') }
-    response.send(data)
-  
-    })
-  })
+    fs.readFile(path.join(__dirname, 'functions_code.js'), 'utf-8', (err, data) => {
+        if (err) return response.send('파일 또는 디렉터리가 잘못됨');
+        response.send(data);
+    });
+});
 
-
+// ID 체크 로직 (수정본)
 app.get('/id/:id', (request, response) => {
-var id_n = request.params;
-let t = 1;
-let response_id;
-while (t != 0) {
-  if(id_n['id'] == accountData[`id${t}`]) {
-    response_id = 'id access'
-    t = 0
-  }
-  else if(t = 10) {
-    response_id = 'id denied'
-  }
-  else; {
-    console.log(t)
-    t = t + 1
-  }
-  response.send(response_id);
-}})
-  
+    const { id } = request.params;
+    let response_id = 'id denied';
+
+    for (let t = 1; t <= 10; t++) {
+        if (id === accountData[`id${t}`]) {
+            response_id = 'id access';
+            break; 
+        }
+    }
+    response.send(response_id);
+});
+
+// Password 체크 로직 (수정본)
 app.get('/password/:password', (request, response) => {
-  var password_n = request.params;
-  let t = 1;
-  let response_password;
-  while (t != 0) {
-    if(password_n['password'] == accountData[`password${t}`]) {
-      response_password = 'password access'
-      t = 0
-    }
-    else if(t = 10) {
-      response_password = 'password denied'
-    }
-    else; {
-      console.log(t)
-      t = t + 1
+    const { password } = request.params;
+    let response_password = 'password denied';
+
+    for (let t = 1; t <= 10; t++) {
+        if (password === accountData[`password${t}`]) {
+            response_password = 'password access';
+            break;
+        }
     }
     response.send(response_password);
-  }})
+});
 
-console.log(accountData)  
+// 데이터 잘 들어왔나 확인용
+console.log("로드된 계정 데이터:", accountData);
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('서버 돌아가는 중: http://localhost:3000');
+});
